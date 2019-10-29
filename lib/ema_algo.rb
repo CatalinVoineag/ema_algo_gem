c4c32dfdd3342fb1c3ea5443c86fda6fdd64ad4d
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2019-10-29 22:33:42 +0000

    Ema Version 3

--- lib/ema_algo.rb
+++ lib/ema_algo.rb
@@ -1,6 +1,7 @@
 require 'ema_algo/version'
 require 'ema_algo/ema_version_1'
 require 'ema_algo/ema_version_2'
+require 'ema_algo/ema_version_3'
 
 module EmaAlgo
 end
---
+++ lib/ema_algo/ema_version_3.rb
@@ -0,0 +1,49 @@
+module EmaAlgo
+  class EmaVersion3
+    attr_reader :low_price, :high_price, :small_ema, :medium_ema, :long_ema, :exit_ema
+    private :low_price, :high_price, :small_ema, :medium_ema, :long_ema, :exit_ema
+
+    def initialize(low_price:, high_price:, small_ema:, medium_ema:, long_ema:, exit_ema:)
+      @low_price, @high_price, @small_ema = low_price, high_price, small_ema
+      @medium_ema, @long_ema, @exit_ema = medium_ema, long_ema, exit_ema
+    end
+
+    def apply
+      if long?
+        'long'
+      elsif short?
+        'short'
+      elsif exit_long?
+        'exit_long'
+      elsif exit_short?
+        'exit_short'
+      else
+        'hold'
+      end
+    end
+
+    def long?
+      small_ema > medium_ema &&
+        small_ema > long_ema &&
+        low_price > medium_ema &&
+        low_price > long_ema
+    end
+
+    def short?
+      small_ema < medium_ema &&
+        small_ema < long_ema &&
+        high_price < medium_ema &&
+        high_price < long_ema
+    end
+
+    def exit_long?
+      exit_ema < medium_ema ||
+        exit_ema < long_ema
+    end
+
+    def exit_short?
+      exit_ema > medium_ema ||
+        exit_ema > long_ema
+    end
+  end
+end
